13986f137f07c50052da7eda95a818904f77ed55
Chaitanya Gayasri <122038270+ChaitanyaGayasri@users.noreply.github.com>
2023-08-24 20:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -88,6 +88,24 @@ state = {blogsData: []}
   }
 
 
+FUNCTION BINDING : 
+
+In JavaScript function binding happens using Bind() method. With this method, we can bind an object to a common function, 
+so that the function gives different results when needed. otherwise, it gives the same result or gives an error while the code is executing. 
+We use the Bind() method to call a function with this value, this keyword refers to the same object which is currently selected. 
+In other words, the bind() method allows us to easily set which object will be bound by this keyword when a function or method is invoked. 
+The need for bind usually occurs, when we use this keyword in a method and we call that method from a receiver object, then sometimes this is not bound to the object that we expect to be bound to. 
+This results in errors in our program. Now, a simple program to print the name which is called by this keyword when the function printFunc() is invoked. 
+
+
+let geeks = {
+    name: "ABC",
+    printFunc: function () {
+        console.log(this.name);
+    }
+}
+  
+geeks.printFunc();
 
 
 2) FEATURES OF DBMS:
